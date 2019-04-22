a380e3724ecb8dc6aa4b20ae49c38319eb92f551
plutoshe <plutoshe@gmail.com>
2019-04-21 22:22:19 -0600

    update scene connection fix

--- Assets/Scripts/Manager/StackSceneManager.cs
+++ Assets/Scripts/Manager/StackSceneManager.cs
@@ -96,6 +96,17 @@ public class StackSceneManager : MonoBehaviour
         }
     }
 
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                // Application.Quit() does not work in the editor so
+                // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                 Application.Quit();
+        #endif
+    }
+
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Scripts/SceneUI/Start/EndGameButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameButtonScript : MonoBehaviour
+{
+
+    void onClick()
+    {
+        StackSceneManager.Instance.QuitGame();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(onClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
